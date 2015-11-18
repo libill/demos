a07a6b76045b8265e6f91d670bd58847269789c4
libill <770737185@qq.com>
2015-11-18 16:34:00 +0800

    use UDP between two phone

--- src/com/libill/demos/activity/UDPActivity.java
+++ src/com/libill/demos/activity/UDPActivity.java
@@ -1,47 +1,62 @@
 package com.libill.demos.activity;
 
 import android.app.Activity;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.libill.demos.R;
 
 public class UDPActivity extends Activity {
-	private Button bt_send_data = null;
-	DatagramSocket socket = null;
-	InetAddress serverAddress = null;
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		bt_send_data = new Button(this);
-		bt_send_data.setText("发送");
-		setContentView(bt_send_data);
-		try {
-			socket = new DatagramSocket(8888);
-			serverAddress = InetAddress.getByName("192.168.1.103");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		bt_send_data.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				new Thread(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							String sendData = "hello world";
-							byte data[] = sendData.getBytes();
-							DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, 8888);
-							socket.send(packet);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}).start();
-			}
-		});
-	}
+    private EditText et_ip;
+    private EditText et_content;
+    private Button bt_send_data;
+    DatagramSocket socket = null;
+    InetAddress serverAddress = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_udp);
+
+        et_ip = (EditText) findViewById(R.id.et_ip);
+        et_content = (EditText) findViewById(R.id.et_content);
+        bt_send_data = (Button) findViewById(R.id.bt_send);
+
+
+        bt_send_data.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if(socket == null) {
+                    try {
+                        socket = new DatagramSocket(8888);
+                        serverAddress = InetAddress.getByName(et_ip.getText().toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String sendData = et_content.getText().toString();
+                            byte data[] = sendData.getBytes();
+                            DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, 8888);
+                            socket.send(packet);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
 }
--- src/com/libill/demos/activity/UDPServerActivity.java
+++ src/com/libill/demos/activity/UDPServerActivity.java
@@ -2,71 +2,106 @@ package com.libill.demos.activity;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class UDPServerActivity  extends Activity {
+import com.libill.demos.R;
 
-	private Button requestButton = null;
+public class UDPServerActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		requestButton = new Button(this);// 作为客户端
-		requestButton.setText("开启");
-		
-		setContentView(requestButton);
-		requestButton.setOnClickListener(new OnClickListener() {
+    private TextView tv_ip;
+    private TextView tv_receive_msg;
+    private Button bt_start;
 
-			@Override
-			public void onClick(View arg0) {
-				requestButton.setText("已开启");
-				new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						DatagramSocket socket = null;
-						try {
-							socket = new DatagramSocket(8888);
-						
-						while (true) {
-							byte data[] = new byte[1024];
-							DatagramPacket packet = new DatagramPacket(data, data.length);
-							System.out.println("result--->2");
-							socket.receive(packet);
-							System.out.println("result--->3");
-							final String result = new String(packet.getData(), packet.getOffset(), packet.getLength());
-							System.out.println("result--->4" + result);
-							
-							runOnUiThread(new Runnable() {
-								
-								@Override
-								public void run() {
-									Toast.makeText(UDPServerActivity.this, result+"", Toast.LENGTH_SHORT).show();
-									
-								}
-							});
-							
-						}
-						
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						
-						
-					}
-				}).start();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_udp);
+        tv_ip = (TextView) findViewById(R.id.tv_ip);
+        tv_receive_msg = (TextView) findViewById(R.id.tv_receive_msg);
+        bt_start = (Button) findViewById(R.id.bt_start);// 作为客户端
 
-			}
+        // 添加滚动属性
+        tv_receive_msg.setMovementMethod(ScrollingMovementMethod.getInstance());
 
-		});
-	}
+        getCurrentIP();
+
+        bt_start.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                bt_start.setText("已开启");
+
+                runUDPServer();
+            }
+
+        });
+    }
+
+    private void runUDPServer(){
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                DatagramSocket socket = null;
+                try {
+                    socket = new DatagramSocket(8888);
+
+                    while (true) {
+                        byte data[] = new byte[1024];
+                        DatagramPacket packet = new DatagramPacket(data, data.length);
+                        socket.receive(packet);
+                        final String result = new String(packet.getData(), packet.getOffset(), packet.getLength());
+                        Log.i("get result:", result);
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Toast.makeText(UDPServerActivity.this, result + "", Toast.LENGTH_SHORT).show();
+                                tv_receive_msg.setText(tv_receive_msg.getText().toString() + "\n" + result);
+                            }
+                        });
+
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void getCurrentIP() {
+        //获取wifi服务
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        //判断wifi是否开启
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ip = intToIp(ipAddress);
+        tv_ip.setText("本机IP地址："+ip);
+    }
+
+    private String intToIp(int i) {
+        return (i & 0xFF) + "." +
+                ((i >> 8) & 0xFF) + "." +
+                ((i >> 16) & 0xFF) + "." +
+                (i >> 24 & 0xFF);
+    }
+
 
 }
