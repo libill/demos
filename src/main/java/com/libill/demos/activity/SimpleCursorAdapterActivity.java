e693c5cbea6ed4a74d43809963a0ba660af429c5
liqiongwei <770737185@qq.com>
2018-09-29 16:34:30 +0800

    request read contacts permission

--- src/main/java/com/libill/demos/activity/SimpleCursorAdapterActivity.java
+++ src/main/java/com/libill/demos/activity/SimpleCursorAdapterActivity.java
@@ -1,25 +1,66 @@
 package com.libill.demos.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Contacts.People;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class SimpleCursorAdapterActivity extends Activity {
-	ListView listView;
+    ListView listView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        listView = new ListView(this);
+        setContentView(listView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SimpleCursorAdapterActivityPermissionsDispatcher.requestPhoneWithPermissionCheck(SimpleCursorAdapterActivity.this);
+    }
+
+    private void setData() {
         //获得一个指向系统通讯录数据库的Cursor对象获得数据来源
         Cursor cur = getContentResolver().query(People.CONTENT_URI, null, null, null, null);
         startManagingCursor(cur);
         //实例化列表适配器
-        listView = new ListView(this);
-        ListAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cur, new String[] {People.NAME}, new int[] {android.R.id.text1});
+        ListAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cur, new String[]{People.NAME}, new int[]{android.R.id.text1});
         listView.setAdapter(adapter);
-        setContentView(listView);
+    }
+
+    @NeedsPermission({Manifest.permission.READ_CONTACTS})
+    public void requestPhone() {
+        setData();
+    }
+
+    @OnPermissionDenied({Manifest.permission.READ_CONTACTS})
+    public void requestPhoneDenied() {
+        Toast.makeText(this, "已禁止该权限", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain({Manifest.permission.READ_CONTACTS})
+    public void requestPhoneNeverAskAgain() {
+        Toast.makeText(this, "请到设置开启权限", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        SimpleCursorAdapterActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 }
