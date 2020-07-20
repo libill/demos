ec6c4d2b2cd17b75116637cfa4366cde9bcdaf8c
libill <770737185@qq.com>
2020-07-20 23:25:11 +0800

    modify AddPhotoBottomDialogFragment

--- app/src/main/java/com/libill/demos/dialog/AddPhotoBottomDialogFragment.kt
+++ app/src/main/java/com/libill/demos/dialog/AddPhotoBottomDialogFragment.kt
@@ -1,12 +1,14 @@
 package com.libill.demos.dialog
 
 import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.WindowManager
 import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import android.view.Gravity
+import androidx.fragment.app.DialogFragment
 import com.libill.demos.R
 import com.libill.demos.databinding.LayoutPhotoBottomSheetBinding
 
@@ -15,20 +17,68 @@ import com.libill.demos.databinding.LayoutPhotoBottomSheetBinding
  * @Author: libill
  * @Date: 2020-07-19 21:32
  */
-class AddPhotoBottomDialogFragment : BottomSheetDialogFragment() {
+class AddPhotoBottomDialogFragment : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return LayoutPhotoBottomSheetBinding.inflate(layoutInflater).root
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val d = super.onCreateDialog(savedInstanceState)
-        //view hierarchy is inflated after dialog is shown
-        d.setOnShowListener {
-            //this disables outside touch
-            //d.window?.findViewById<View>(R.id.touch_outside)?.setOnClickListener(null)
-            //this prevents dragging behavior
-            (d.window?.findViewById<View>(R.id.design_bottom_sheet)?.layoutParams as CoordinatorLayout.LayoutParams).behavior = null
+        val dialog = Dialog(activity!!, R.style.BottomDialog)
+        dialog.setCanceledOnTouchOutside(true);
+        return dialog
+    }
+
+    private fun fullScreenImmersive(view: View?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    )
+            view?.systemUiVisibility = uiOptions
         }
-        return d
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setWindowParams()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        view?.postDelayed({
+            setWindowParams()
+        }, 200)
+    }
+
+    private fun setWindowParams() {
+        val window = dialog?.window
+        // 全屏隐藏导航栏
+        window?.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+        fullScreenImmersive(window?.decorView)
+        window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+
+        // 背景透明
+        val windowParams = window?.attributes
+        windowParams!!.dimAmount = 0.0f
+        windowParams.width = WindowManager.LayoutParams.MATCH_PARENT
+        windowParams.gravity = Gravity.BOTTOM
+        window.attributes = windowParams
+
+//        dialog?.window?.run {
+//            // 全屏隐藏导航栏
+//            setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+//            fullScreenImmersive(decorView)
+//            clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+//
+//            // 背景透明
+//            attributes.run {
+//                dimAmount = 0.0f
+//                width = WindowManager.LayoutParams.MATCH_PARENT
+//                gravity = Gravity.BOTTOM
+//            }
+//        }
     }
 }
